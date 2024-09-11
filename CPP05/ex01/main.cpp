39aec51f4cd5793f18f4de41726ccdb0d947f7e3
Shahria Mohammed <shmohamm@lab1r1s7.42abudhabi.ae>
2024-09-11 10:51:35 +0400

    changed main  for CPP05-ex01

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shmohamm <shmohamm@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:48:22 by shmohamm          #+#    #+#             */
-/*   Updated: 2024/09/10 12:48:23 by shmohamm         ###   ########.fr       */
+/*   Updated: 2024/09/11 10:49:56 by shmohamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 #include "Form.hpp"
 #include <iostream>
 
-
-
 int main()
 {
 	try{
-		std::cout << "" << std::endl;
+		std::cout << std::endl;
 		Bureaucrat* ab = new Bureaucrat("b1", 150);
-		std::cout << "" << std::endl;
+		std::cout << std::endl;
 		Bureaucrat* ac = new Bureaucrat("b2", 20);
-		std::cout << "" << std::endl;
+		std::cout << std::endl;
 		Form* cd = new Form("f1", 30, 10);
 
 		std::cout <<  std::endl << *cd << std::endl;
 		try {
 			ab->signForm(*cd);
-			std::cout << "" << std::endl;
+			std::cout << std::endl;
 		} catch (std::exception& e) {
 			std::cout << e.what() << std::endl;
 		}
 		std::cout << std::endl << *cd << std::endl;
 		try {
 			cd->beSigned(*ac);
-			std::cout << "" << std::endl;
+			std::cout << std::endl;
 		} catch (std::exception& e) {
 			std::cout << e.what() << std::endl;
 		}
